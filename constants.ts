8be710d85d797d6aa6c91f904a7ac284cd0f3404
Bgsitopu <habelsitopu1@gmail.com>
2025-10-28 04:47:56 +0700

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,28 @@
+
+import { Student } from './types';
+
+export const INITIAL_STUDENTS: Student[] = [
+];
+
+export const KELAS_OPTIONS = ["7A", "7B", "7C", "Kelas Mensetsu"];
+
+export const GENDER_OPTIONS = [
+  { value: "L", label: "Laki-laki" },
+  { value: "P", label: "Perempuan" },
+];
+
+export const SSW_OPTIONS = [
+  "Kaigo",
+  "Pengolahan Makanan",
+  "Restoran",
+  "Pertanian",
+  "Konstruksi",
+  "Pembersihan Gedung",
+  "Perikanan",
+  "Industri Mesin",
+  "Elektronik",
+  "Tekstil",
+  "Pembuatan Logam",
+  "Peternakan",
+  "Belum ada"
+];
